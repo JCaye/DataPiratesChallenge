ff2616b29813feaf31eae77c62c8bc9ecc29f779
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-12-10 15:59:51 -0200

    fixed test files name

--- src/cep/test_buscaCep.py
+++ src/cep/test_buscaCep.py
@@ -2,7 +2,7 @@ import pytest
 
 from buscaCep import buildFormData, getDataFromUrl, readDataFromHtmlTable, writeToFile
 
-def buildFormData_whenValidArgs_thenSuccess():
+def test_buildFormData_whenValidArgs_thenSuccess():
 	assert buildFormData('SC', 50) == {
 									'UF': 'SC',
 									'qtdrow': 50
